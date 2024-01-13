431b59260d1d206b51dd0df567cd12599313ffcf
mugidar <mugidar@gmail.com>
2024-01-13 20:58:42 +0300

    init

---
+++ frontend/views/hello/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'Hello';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1>HELLOOOOO</h1>
+</div>
